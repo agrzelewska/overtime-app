e67dc816ae73d351659c7d0d0d710c68b4a7c08f
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-11-20 13:48:57 +0100

    Use the resource generator for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'creatrspecion' do
-    before do
-      @user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
-    end
+  before do
+    @user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jon', last_name: 'Snow')
+  end
 
+  describe 'creation' do
     it 'can be created' do
       expect(@user).to be_valid
     end
 
+  describe 'validations' do
     it 'cannot be created without first name and last name' do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
+  end
   end
 end
