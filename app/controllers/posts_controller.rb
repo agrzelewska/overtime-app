325e4ac4a41929c0f20372477f1288ca17948c82
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-11-25 15:52:43 +0100

    Implement creation methods for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: %i[show]
 
+  def index; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+
+    if @post.save
+      redirect_to @post, notice: "Your post was created successfully"
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
