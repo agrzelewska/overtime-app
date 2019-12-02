5dd4355c0c34ec13b9124ba24a56c6c186a045e8
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-12-02 12:29:01 +0100

    Integrate permission structures to hide posts from non authorized users

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'navigate', type: :feature do
   before do
     @user = FactoryBot.create(:user)
     login_as(@user, :scope => :user)
+    @post1 = create(:post, user_id: @user.id)
+    @post2 = create(:second_post, user_id: @user.id)
   end
 
   describe 'index' do
@@ -19,10 +21,15 @@ RSpec.describe 'navigate', type: :feature do
     end
 
     it 'has a list of posts' do
-      post1 = FactoryBot.create(:post)
-      post2 = FactoryBot.create(:second_post)
       visit posts_path
       expect(page).to have_content(/rationale|content/)
+    end
+
+    it 'has a scope so that only post creators can see their posts' do
+      admin_user = FactoryBot.create(:admin_user)
+      post_from_admin_user = Post.create(date: Date.today, rationale: 'Admin post', user_id: admin_user.id)
+      visit posts_path
+      expect(page).to_not have_content(/Admin/)
     end
   end
 
@@ -36,9 +43,8 @@ RSpec.describe 'navigate', type: :feature do
 
   describe 'delete' do
     it 'can be deleted' do
-      @post = FactoryBot.create(:post)
       visit posts_path
-      click_link("delete_#{@post.id}")
+      click_link("delete_#{@post1.id}")
       expect(page.status_code).to eq(200)
     end
   end
@@ -67,24 +73,19 @@ RSpec.describe 'navigate', type: :feature do
   end
 
   describe 'edit' do
-    before do
-      @post = FactoryBot.create(:post)
-      @post.user_id = @user.id
+    it 'can be edited' do
+      visit edit_post_path(@post1)
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited content"
+      click_on 'Save'
+      expect(page).to have_content("Edited content")
     end
-
-    # it 'can be edited' do
-    #   visit edit_post_path(@post)
-    #   fill_in 'post[date]', with: Date.today
-    #   fill_in 'post[rationale]', with: "Edited content"
-    #   click_on 'Save'
-    #   expect(page).to have_content("Edited content")
-    # end
 
     it 'cannot be edited by a non authorized user' do
       logout(:user)
       non_authorized_user = FactoryBot.create(:user)
       login_as(non_authorized_user, scope: :user)
-      visit edit_post_path(@post)
+      visit edit_post_path(@post1)
       expect(current_path).to eq(root_path)
     end
   end
