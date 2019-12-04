23276b014b2463f2999bacd0008be542816726bc
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-12-04 15:41:24 +0100

    Validate AuditLog

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,16 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :user, :status, :start_date
+
+  validate :start_date_6_days_prior
+
+  private
+
+  def start_date_6_days_prior
+    if start_date != Date.current - 6
+      errors.add(:start_date, "it is not valid")
+    end
+  end
+
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:audit_log) { create(:audit_log) }
+
+  describe 'creation' do
+    it 'can be properly created' do
+      expect(audit_log).to be_valid
+    end
+  end
+
+  describe 'validation' do
+    it 'should require to have a user association' do
+      audit_log.user = nil
+      expect(audit_log).to_not be_valid
+    end
+
+    it 'should always have a status' do
+      audit_log.status = nil
+      expect(audit_log).to_not be_valid
+    end
+
+    it 'should require to have a start date' do
+      audit_log.start_date = nil
+      expect(audit_log).to_not be_valid
+    end
+
+    it 'should have a start date equal to 6 days prior' do
+      audit_log.start_date = Date.today - 12
+      expect(audit_log).to_not be_valid
+    end
+
+  end
 end
