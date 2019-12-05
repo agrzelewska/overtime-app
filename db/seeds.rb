52816b2390372fc1e198370751ffc3e11251cb95
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-12-05 14:27:56 +0100

    Amend admin users and audit logs seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,34 @@
 
 puts '1 User created'
 
-AdminUser.create(email: 'admin@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Jonathan', last_name: 'Winter')
+AdminUser.create(email: 'admin@test.com',
+                 password: 'asdfasdf',
+                 password_confirmation: 'asdfasdf',
+                 first_name: 'Jonathan',
+                 last_name: 'Winter',
+                 phone: '432432432')
 
 puts '1 AdminUser created'
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create(date: Date.today,
+              rationale: "#{post} rationale content",
+              user_id: @user.id,
+              overtime_request: 2.5)
 end
 
 puts "100 Posts have  been created"
 
-100.times do |audit_log|
-  AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6))
+6.times do |audit_log|
+  AuditLog.create(user_id: @user.id,
+                  status: 0,
+                  start_date: (Date.today - 6))
 end
 
-puts "100 AuditLogs have been created"
+4.times do |audit_log|
+  AuditLog.create(user_id: @user.id,
+                  status: 1,
+                  start_date: (Date.today - 6))
+end
+
+puts "10 AuditLogs have been created"
