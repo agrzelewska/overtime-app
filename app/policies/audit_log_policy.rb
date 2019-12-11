dd18c1aa5de73f2da84b360b905d6c3b587919d1
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-12-11 16:18:58 +0100

    mImplement functional buttons for employee homepage confirmations

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -3,9 +3,17 @@ class AuditLogPolicy < ApplicationPolicy
     return true if admin?
   end
 
+  def confirm?
+    user_or_admin
+  end
+
   private
 
   def admin?
     admin_types.include?(user.type)
+  end
+
+  def user_or_admin
+    user.id == record.user_id || admin?
   end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe 'homepage' do
     expect(post.reload.status).to eq('approved')
   end
 
+  it 'allows the employee to change the audit log status from the homepage' do
+    user = create(:user)
+    login_as(user, scope: :user)
+    audit_log = create(:audit_log)
+    audit_log.update(user_id: user.id)
+
+    visit root_path
+    click_on("confirm_#{audit_log.id}")
+    expect(audit_log.reload.status).to eq('confirmed')
+  end
+
 end
