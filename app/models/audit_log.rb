38bc774eac73c6dd4a4518bafbcb299a691ab72e
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-12-13 10:27:29 +0100

    Add update callback for audit logs to set confirmed date

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -7,11 +7,17 @@ class AuditLog < ApplicationRecord
 
   after_initialize :set_defaults
   # validate :start_date_6_days_prior
+  #
+  before_update :set_end_date, if: :confirmed?
 
   private
 
   def set_defaults
     self.start_date ||= Date.today - 6.days
+  end
+
+  def set_end_date
+    self.end_date = Date.today
   end
   # def start_date_6_days_prior
   #   if start_date != Date.current - 6
